c6a0fbdf61da6b46cbbbda94535063525779eb13
usamimi39 <usamin04tp@gmail.com>
2026-01-03 00:15:11 +0900

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="fixed top-6 left-0 right-0 h-14 z-50 w-full max-w-7xl mx-auto px-4 bg-white shadow-md rounded-full">
+      <div className="max-w-7xl mx-auto py-6 px-4"></div>
+    </header>
+  );
+}
