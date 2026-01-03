78925bf29271d2176dd610de255ca5cac482081d
usamimi39 <usamin04tp@gmail.com>
2026-01-03 20:41:16 +0900

    add-grid-and-svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@ import Image from "next/image";
 import Header from "../components/Header";
 
 export default function Home() {
-  const gridPattern = `url("data:image/svg+xml,%3Csvg width='40' height='40' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M 40 0 L 0 0 0 40' fill='none' stroke='%23d1d5db' stroke-width='1'/%3E%3C/svg%3E")`;
-  
   return (
-    <div
-      className="min-h-screen bg-slate-50">
-      <header className="flex min-h-screen items-center justify-center">
-        <><Header /></>
-      </header>
+    <div className="relative min-h-screen bg-gray-100">
+      <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#fafafa_1px,transparent_1px),linear-gradient(to_bottom,#fafafa_1px,transparent_1px)] bg-[size:24px_24px]" />
+      <Header />
+      <main className="flex min-h-screen items-center justify-center">
+        {/* Content here */}
+      </main>
     </div>
   );
 }
